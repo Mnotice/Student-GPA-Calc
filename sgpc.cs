0dc61a14cd0c44244de20dcd62483d5c10a5fc6d
Mikhail <mikhail1337@hotmail.com>
2023-04-05 09:56:10 +0200

    Update sgpc.cs

--- sgpc.cs
+++ sgpc.cs
@@ -55,5 +55,4 @@ Console.WriteLine($"{course2Name}\t\t\t{course2Grade}\t\t{course2Credit}");
 Console.WriteLine($"{course3Name}\t\t\t{course3Grade}\t\t{course3Credit}");
 Console.WriteLine($"{course4Name}\t{course4Grade}\t\t{course4Credit}");
 Console.WriteLine($"{course5Name}\t\t{course5Grade}\t\t{course5Credit}");
-
 Console.WriteLine($"\nFinal GPA:\t\t\t{leadingDigit}.{trailingDigits}");
